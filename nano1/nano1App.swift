240601807b4925fd8e7a5f14b8765ece9f44aaa8
joshuawenata <joshuawenata@gmail.com>
2024-04-23 12:21:10 +0700

    Initial Commit

---
+++ nano1/nano1App.swift
@@ -0,0 +1,21 @@
+//
+//  nano1App.swift
+//  nano1
+//
+//  Created by Joshua Wenata Sunarto on 23/04/24.
+//
+
+import SwiftUI
+
+@main
+struct nano1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+
+        ImmersiveSpace(id: "ImmersiveSpace") {
+            ImmersiveView()
+        }
+    }
+}
