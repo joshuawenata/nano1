6574733d8910f6f572862d359dc321ad4ca1f719
joshuawenata <joshuawenata@gmail.com>
2024-04-25 20:46:34 +0700

    add background and 3d obj

--- nano1/Component/Card.swift
+++ nano1/Component/Card.swift
@@ -18,22 +18,25 @@ struct Card: View {
     @Environment(\.dismissImmersiveSpace) var dismissImmersiveSpace
     
     var body: some View {
-        HStack {
-            Image(card.image)
-                .resizable()
-                .padding()
-                .frame(width: 200, height: 200)
-                .border(Color.white, width: 20)
+        HStack(alignment: .center) {
             
             VStack {
+                Image(card.image)
+                    .resizable()
+                    .frame(width: 200, height: 200)
+                    .cornerRadius(20)
                 Button(action: {
                     card.action()
                 }, label: {
-                    Text("Show Immersive Space")
+                    //show sf symbols
+                    Image(systemName: "play.circle")
+                        .font(.system(size: 50))
                 })
+                .buttonStyle(PlainButtonStyle())
+                .padding()
             }
-            .padding(.leading, 50)
-            .frame(width: 400)
+            .padding()
+            
         }
         .padding(20)
         .background(Color.white.opacity(0.2))
